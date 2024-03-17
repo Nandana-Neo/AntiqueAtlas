3a9cd68f7f1a1058dbec1417d52c214b56000d79
Nandana <129848120+Nandana-Neo@users.noreply.github.com>
2024-03-17 11:12:08 +0530

    Delete Project directory

--- Project/profile.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call %APPDATA%\Python\Scripts\virtualenvwrapper.bat
--- Project/project1/history/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import *
-class Contactform(ModelForm):
-    class Meta:
-        model=Contacts
-        fields='__all__'
-
-
-        
